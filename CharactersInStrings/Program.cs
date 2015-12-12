bb63569ba5264d6e1973f861d5d84f37bc91e3f8
Eugene <yeug@gmail.com>
2015-12-12 23:37:43 +0200

    wanted to use string as an array

---
+++ CharactersInStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplacingSymbols
+{
+    class Program
+    {
+        //Написать метод, входными параметрами которого являются строка,
+        //два символа. Метод должен заменить в строке все вхождения первого символа на символ «*» если рядом с этим символом не стоит второй символ.
+        public string ReplaceSymbols(string inputLine, string characters)
+        {
+            string outputLine = null;
+            for (int i = 0; i < (inputLine.Length - 1); i++)
+            {
+                if (inputLine[i] == characters[0])
+                {
+                    if (inputLine[i + 1] != characters[1])
+                    {
+                        outputLine[i] = "*";
+                    }
+                }
+            }
+            return outputLine;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
